ab46d3dca0becdb13b257c5ed7f4bcf1e9a2f2b0
Dean Kruger <dean.kruger3@gmail.com>
2026-08-09 13:11:55 +0200

    chore(portfolio): remove superseded CV implementations

--- cv/main.js
+++
@@ -1,7 +0,0 @@
-import { EditorToolbar } from './components/editor-toolbar.js';
-import { PageOne } from './components/page-one.js';
-const pageRoot = document.querySelector('#page-one');
-if (pageRoot) {
-    const page = new PageOne(pageRoot);
-    new EditorToolbar(page);
-}
